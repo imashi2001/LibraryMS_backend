ebaf57ddb79ec0831c7d793fba76d158c6bd3600
imashi2001 <ranaweeraimashi880@gmail.com>
2025-12-07 09:38:06 +0530

    Backend Maven App setup done

---
+++ backend/src/test/java/com/imashi/lms/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imashi.lms.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
